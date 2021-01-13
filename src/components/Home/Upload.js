e6ca3721dee2a2254c2b1318109ff25e9a4bea05
AntoineGGG <a.gorenflot@gmx.fr>
2021-01-13 20:21:55 +0100

    upload image ok

--- src/components/Home/Upload.js
+++ src/components/Home/Upload.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import Modal from './Modal';
+import React, { useState } from 'react';
+/* import Modal from './Modal'; */
 import './Upload.scss';
 import axios from 'axios';
 
@@ -7,13 +7,14 @@ const Upload = () => {
   const [posts, setPosts] = useState([]);
   const [title, setTitle] = useState('Enter your Name');
   const [mainPicture, setMainPicture] = useState(null);
+  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     axios
       .get('http://localhost:5000/posts')
       .then((res) => res.data)
       .then((data) => setPosts(data));
-  }, []);
+  }, []); */
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -21,14 +22,15 @@ const Upload = () => {
     formData.append('title', title);
     formData.append('main_picture', mainPicture);
 
-    axios
-      .post('http://localhost:5000/posts', formData)
-      .then((res) => res.data)
-      .then((data) =>
-        setPosts([...posts, { title, main_picture_url: data.main_picture_url }])
-      );
+    axios.post('http://localhost:5000/uploaddufichier', formData).then((res) =>
+      setUsers({
+        name: res.data.name,
+        avatar: `http://localhost:5000/${res.data.path}`,
+      })
+    );
   };
 
+  console.log(users);
   return (
     <div>
       <h2>Welcome On Board !</h2>
@@ -48,6 +50,9 @@ const Upload = () => {
         <input type='submit' value='Create Your Avatar' />
         <p>how does it work ?</p>
       </form>
+      <div>
+        <img src={`${users.avatar}`} alt='coucou' />
+      </div>
     </div>
   );
 };
