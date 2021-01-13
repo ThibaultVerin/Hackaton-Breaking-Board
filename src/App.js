e34256b8ab72e28dd7230a27d0130537edb565c4
AntoineGGG <a.gorenflot@gmx.fr>
2021-01-13 19:08:56 +0100

    init

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import './App.scss';
 import Home from './components/Home/Home.js';
 
 function App() {
-  return (
-    <div className='App'>
-      <Home />
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
