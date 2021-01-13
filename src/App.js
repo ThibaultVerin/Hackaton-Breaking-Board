da8348c38383824b87af3027fd6f1ffc875e0787
AntoineGGG <a.gorenflot@gmx.fr>
2021-01-13 20:34:36 +0100

    again eslint pb

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.scss';
 import Home from './components/Home/Home.js';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
