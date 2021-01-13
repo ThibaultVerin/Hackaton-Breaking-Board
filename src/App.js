e97886f771b9d7534a7c325e3f29ae787c151ff5
Jean Ducret <jean.dcrt@gmail.com>
2021-01-13 16:04:44 +0100

    create board and a beautiful little wall

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React, { useState } from 'react';
 import './App.scss';
+import { drawBoard, wall, createBoard } from './components/Board/Board';
 import Home from './components/Home/Home.js';
 
 function App() {
+  const [board, setBoard] = useState(createBoard(wall));
+  console.log(board);
   return (
     <div className='App'>
-      <Home />
+      <div className='board-container'>{drawBoard(board)}</div>
     </div>
   );
 }
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,8 +1,50 @@
-import React from 'react';
 import './Board.scss';
+export const createEmptyBoard = () => {
+  const BOARD_SIZE = 10;
+  const board = new Array(BOARD_SIZE);
 
-const Board = () => {
-  return <div>Salut les chinchilles</div>;
+  for (let x = 0; x < BOARD_SIZE; x++) {
+    board[x] = new Array(BOARD_SIZE);
+    for (let y = 0; y < BOARD_SIZE; y++) {
+      board[x][y] = {
+        x,
+        y,
+        isPlayer: false,
+        isWall: false,
+      };
+    }
+  }
+  return board;
 };
 
-export default Board;
+export const drawBoard = (board) => {
+  return board.map((row) => {
+    return row.map((cell, index) => {
+      return <div key={index} className={cell.isWall ? 'wall' : 'cell'}></div>;
+    });
+  });
+};
+
+export const populateWithWall = (board, wall) => {
+  board.forEach((row) => {
+    row.forEach((cell) => {
+      wall.forEach((wCell) => {
+        if (wCell.x === cell.x && wCell.y === cell.y) {
+          console.log('mur');
+          return (cell.isWall = true);
+        }
+      });
+    });
+  });
+};
+
+export const createBoard = (wall) => {
+  const b = createEmptyBoard();
+  populateWithWall(b, wall);
+  return b;
+};
+
+export const wall = [
+  { x: 0, y: 4 },
+  { x: 1, y: 4 },
+];
