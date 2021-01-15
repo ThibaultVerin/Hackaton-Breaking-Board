6dd6d5a038f991a03bd9043d72a02c0d70b3a0ff
Thibault Verin <thibaultverin@hotmail.com>
2021-01-15 04:10:37 +0100

    Launch stable version of action part

--- src/components/Action/Action.js
+++ src/components/Action/Action.js
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
 import './Action.scss';
 
 const Action = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
   const {
     users,
     setUsers,
     currentUser,
     setCurrentUser,
-    isCoffeTaken,
+    isCoffeeTaken,
     setIsCoffeeTaken,
     playerShot,
     socket,
@@ -26,9 +28,26 @@ const Action = () => {
     userTemp[1].x = 3;
     userTemp[1].y = 2;
 
+    setIsClicked(true);
     setIsCoffeeTaken(true);
     setUsers(userTemp);
     console.log(users);
+  };
+
+  const handleGame = () => {
+    const currentUserTemp = [];
+
+    const userTemp = [];
+    users.forEach((e) => {
+      userTemp.push({ name: e.name, avatar: e.avatar, id: e.id });
+    });
+    userTemp[0].x = 0;
+    userTemp[0].y = 7;
+    userTemp[1].x = 0;
+    userTemp[1].y = 9;
+
+    setIsClicked(true);
+    setUsers(userTemp);
   };
 
   const shootPlayer = () => {
@@ -60,14 +79,18 @@ const Action = () => {
   };
 
   return (
-    <div>
-      <h3>Choose Your Action</h3>
-      <div>
-        <div onClick={handleCoffee}>Drink Coffee</div>
-        <div onClick={shootPlayer}>SHOOT </div>
-        <div>Challenge + name</div>
-      </div>
-    </div>
+    <>
+      {!isClicked && (
+        <div className='actionMenu'>
+          <h3>Choose Your Action</h3>
+          <div className='actionLink'>
+            <p onClick={handleCoffee}>Drink Coffee</p>
+            <p onClick={shootPlayer}>SHOOT </p>
+            <p onClick={handleGame}>Challenge + name</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
