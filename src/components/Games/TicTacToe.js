32ac32e5cfe234217db32f403b468f4c6570ac40
Nathan-Vanstaevel <nathanvanstaevel@gmail.com>
2021-01-15 11:28:41 +0100

    add dot background

---
+++ src/components/Games/TicTacToe.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import GetUserDetail from './GetUserDetail';
+import ShowUsers from './ShowUsers';
+import GamePlay from './GamePlay';
+import { Container } from 'react-bootstrap';
+import socketIOClient from 'socket.io-client';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: 'http://127.0.0.1:4444',
+      socket: null,
+      isGameStarted: false,
+      gameId: null,
+      gameData: null,
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    // Made a connection with server
+    const socket = socketIOClient(endpoint);
+    socket.on('connected', (data) => {
+      this.setState({ socket: socket });
+    });
+  }
+  registrationConfirmation = (data) => {
+    // If registration successfully redirect to player list
+    this.setState({ isRegistered: data });
+  };
+  gameStartConfirmation = (data) => {
+    // If select opponent player then start game and redirect to game play
+    this.setState({
+      isGameStarted: data.status,
+      gameId: data.game_id,
+      gameData: data.game_data,
+    });
+  };
+  opponentLeft = (data) => {
+    // If opponent left then get back from game play to player screen
+    alert('Opponent Left');
+    this.setState({ isGameStarted: false, gameId: null, gameData: null });
+  };
+  render() {
+    return (
+      <Container>
+        {!this.state.isGameStarted ? (
+          !this.state.isRegistered ? (
+            <header className='App-header'>
+              {this.state.socket ? (
+                <GetUserDetail
+                  socket={this.state.socket}
+                  registrationConfirmation={this.registrationConfirmation}
+                />
+              ) : (
+                <p>Loading...</p>
+              )}
+            </header>
+          ) : (
+            <ShowUsers
+              socket={this.state.socket}
+              gameStartConfirmation={this.gameStartConfirmation}
+            />
+          )
+        ) : (
+          <GamePlay
+            socket={this.state.socket}
+            gameId={this.state.gameId}
+            gameData={this.state.gameData}
+            opponentLeft={this.opponentLeft}
+          />
+        )}
+      </Container>
+    );
+  }
+}
+
+export default App;
