33bc14bb6d6986e5c831488a96f0d213b6711aca
Thibault Verin <thibaultverin@hotmail.com>
2021-01-15 12:03:01 +0100

    Adding style to the current user part

--- src/components/Action/Action.js
+++ src/components/Action/Action.js
@@ -42,7 +42,7 @@ const Action = () => {
       id: playerShot.id,
       x: playerShot.x,
       y: playerShot.y,
-      life: playerShot.life - 10,
+      life: playerShot.life - 30,
       nerf: playerShot.nerf,
     };
 
